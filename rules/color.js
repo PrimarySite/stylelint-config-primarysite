ce46efccf52afa274ffc65516dbac43ab57748b0
SammyM <sammy.marsden@primarysite.net>
2017-07-11 15:25:35 +0100

    Added some colour rules and made stylelint a peer dependency

--- rules/color.js
+++ rules/color.js
@@ -1,5 +1,9 @@
 module.exports = {
   rules: {
-    'color-hex-case': 'lower'
+    'color-hex-case': 'lower',
+    'color-hex-length': 'short',
+    'color-named': 'never',
+    'color-no-hex': 'off',
+    'color-no-invalid-hex': true
   }
 };
