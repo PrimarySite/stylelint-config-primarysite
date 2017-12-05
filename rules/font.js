790158f4dac99dda81f5110c49a03c2b8077a6ae
SammyM <sammy.marsden@primarysite.net>
2017-12-05 11:50:55 +0000

    Added some rules and bumped the peer dependency version of stylelint

--- rules/font.js
+++ rules/font.js
@@ -2,6 +2,7 @@ module.exports = {
   rules: {
     'font-family-name-quotes': 'always-where-recommended',
     'font-family-no-duplicate-names': true,
+    'font-family-no-missing-generic-family-keyword': true,
     'font-weight-notation': 'named-where-possible'
   }
 };
--- rules/unit.js
+++ rules/unit.js
@@ -1,6 +1,13 @@
 module.exports = {
   rules: {
-    'unit-blacklist': ['ch', 'cm', 'em', 'in', 'mm', 'pc', 'pt'],
+    'unit-blacklist': [
+      ['ch', 'cm', 'em', 'in', 'mm', 'pc', 'pt'],
+      {
+        ignoreMediaFeatureNames: {
+          'em': ['/height$/', '/width$/']
+        }
+      }
+    ],
     'unit-case': 'lower',
     'unit-no-unknown': true,
     'unit-whitelist': null
